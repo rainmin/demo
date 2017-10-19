d7cd4ece32bbf558a0c703f00e91fd0ba0960840
Rainmin Chan <rainminchan@gmail.com>
2017-10-19 11:19:50 +0800

    add BaseActivity

---
+++ app/src/main/java/com/rainmin/demo/BaseActivity.java
@@ -0,0 +1,56 @@
+package com.rainmin.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class BaseActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    Toolbar toolbar;
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (unbinder != null)
+            unbinder.unbind();
+    }
+
+    /**
+     * the inheriting class should call this function in onCreate, to add customized view
+     * and initialize ButterKnife
+     * @param resId customized view id
+     * @param title title displayed on toolbar
+     */
+    public void initCustomizedView(int resId, String title) {
+        setContentView(R.layout.activity_base);
+        frameLayout = (FrameLayout) findViewById(R.id.content_layout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // add customized view
+        View contentView = View.inflate(this, resId, null);
+        frameLayout.addView(contentView);
+        // add annotation framework
+        unbinder = ButterKnife.bind(this);
+        // init toolbar
+        toolbar.setTitle(title);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/rainmin/demo/noticeboard/NoticeboardActivity.java
@@ -0,0 +1,46 @@
+package com.rainmin.demo.noticeboard;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.rainmin.demo.BaseActivity;
+import com.rainmin.demo.R;
+import com.rainmin.noticeboard.NoticeBoard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * show how to use NoticeBoard control
+ */
+public class NoticeboardActivity extends BaseActivity {
+
+    @BindView(R.id.notice_board)
+    NoticeBoard noticeBoard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initCustomizedView(R.layout.activity_noticeboard, "公告栏");
+        // init noticeboard
+        List<String> notices = new ArrayList<>(3);
+        notices.add("网络安全周最强厂商巡礼：腾讯安全生态舰队全军出击");
+        notices.add("高精地图对自动驾驶有多重要？和一般导航地图有何区别？");
+        notices.add("15部委：2020年全国基本实现车用乙醇汽油全覆盖");
+        noticeBoard.setNotices(notices)
+                .setFlipInterval(2000)
+                .setTextSize(20)
+                .setItemClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String notice = ((TextView) v).getText().toString();
+                        Toast.makeText(NoticeboardActivity.this, notice, Toast.LENGTH_SHORT).show();
+                    }
+                }).start();
+    }
+
+}
